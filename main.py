9c5d36b4d338534269600e11e998d003e89fa91a
Polina Bukina <pbukina501@gmail.com>
2025-01-25 16:44:05 +0300

    Добавила основной код в файл main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('Hello_World')
+from utils import add_numbers, multiply_numbers
+if __name__ == "__main__":
+  a = 5
+  b = 3
+  x = 2
+  y = 5
+  sum = add_numbers(x, y)
+  multiply = multiply_numbers(x, y)
+  ptint(f"Сумма: {sum}")
+  print(f"Произведение: {product_result}")
